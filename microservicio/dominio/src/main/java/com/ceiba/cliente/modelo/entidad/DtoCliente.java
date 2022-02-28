2f606549d8433f2394548f760521610a9ebe1e0b
Duval <deledesman@gmail.com>
2022-02-28 16:14:46 -0500

    Configuración de exclusiones sonar

--- microservicio/dominio/src/main/java/com/ceiba/cliente/modelo/entidad/DtoCliente.java
+++ microservicio/dominio/src/main/java/com/ceiba/cliente/modelo/entidad/DtoCliente.java
@@ -2,10 +2,13 @@ package com.ceiba.cliente.modelo.entidad;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class DtoCliente {
     private Long id;
     private String tipoIdentificacion;
